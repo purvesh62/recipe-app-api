41bf8a2444fced44a763354b95b1877783ad88c9
Purvesh Rathod <purvesh62@gmail.com>
2024-01-26 15:02:38 -0400

    Added user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,34 @@
-from django.db import models # noqa
+"""
+Database models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractUser,  # Provides functionality for the authentication system.
+    BaseUserManager,  # A base user manager class provided by Django.
+    PermissionsMixin  # Provides functionality for the permissions and fields system.
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save, and return user."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractUser, PermissionsMixin):
+    """User model"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'  # Defines field for authentication. Defaults is user_id.
